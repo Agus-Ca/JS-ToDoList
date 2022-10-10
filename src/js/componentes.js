49ae5e166b5f053f71065195bd07c05b33409761
Agustín Casado <agu.e.casado@gmail.com>
2022-10-10 14:50:00 -0300

    eliminar ToDos completados

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -6,6 +6,7 @@ import { todoList } from '../index';
 // referencias en el HTML
 const divTodoList = document.querySelector('.todo-list');
 const txtInput    = document.querySelector('.new-todo');
+const btnBorrar   = document.querySelector('.clear-completed');
 
 
 
@@ -59,5 +60,14 @@ divTodoList.addEventListener('click', ( event ) => {
     if ( nombreElemento.includes('button') ){
         todoList.eliminarTodo( todoId );
         divTodoList.removeChild( todoElemento );
+    }
+});
+
+btnBorrar.addEventListener('click', () => {
+    todoList.eliminarCompletados();
+    for( let i = divTodoList.children.length - 1; i >= 0; i--) {
+        const elemento = divTodoList.children[i];
+        if ( elemento.classList.contains('completed') );
+        divTodoList.removeChild( elemento );
     }
 });
