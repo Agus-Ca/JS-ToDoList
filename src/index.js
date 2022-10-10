c9e8962546a4d2d87e7c56ac6aea0081e173144c
Agustín Casado <agu.e.casado@gmail.com>
2022-10-10 13:53:19 -0300

    construir tareas en el HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 import './styles.css';
+
+
+
+const todoList = new TodoList();
+const tarea = new Todo('Aprender JS');
+todoList.nuevoTodo( tarea );
+
+console.log( todoList );
+
+crearTodoHtml( tarea );
